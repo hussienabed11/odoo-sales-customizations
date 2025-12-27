d125b55ca9df6a624f7e59b3fee22058630a0d76
Hussein <hussienabed1111@gmail.com>
2025-12-27 22:38:58 +0200

    Add computed field and onchange for priority discount
    - Added `discount_amount` to auto-calculate discount for priority orders.
    - Added onchange on `is_priority` to help user with data entry.

--- sale_custom/models/sale_order.py
+++ sale_custom/models/sale_order.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 
@@ -6,6 +6,24 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     is_priority = fields.Boolean(string="Priority Order")
+    discount_amount = fields.Float(string="Priority Discount", compute="_compute_discount")
+
+    @api.depends('is_priority', 'amount_total')
+    def _compute_discount(self):
+        for order in self:
+            if order.is_priority:
+                order.discount_amount = order.amount_total * 0.05
+            else:
+                order.discount_amount = 0.0
+    @api.onchange('is_priority')
+    def _onchange_is_priority(self):
+        if self.is_priority:
+            return {
+                'warning': {
+                    'title': "Priority Order",
+                    'message': "This order is marked as priority! Discount will be applied automatically."
+                }
+            }
 
     def action_mark_as_priority(self):
         for order in self:
@@ -19,10 +37,7 @@ class SaleOrder(models.Model):
                 body="This sale order has been marked as PRIORITY."
             )
 
-
     def action_confirm(self):
-
-
         for order in self:
             if order.is_priority and order.amount_total < 10000:
                 raise UserError(
